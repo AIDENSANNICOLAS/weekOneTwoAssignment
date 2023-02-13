ed7186bf902887b6fce6981d2e8c4a0a7368ad77
AIDENSANNICOLAS <sannicoa@wlhs.wlwv.k12.or.us>
2023-02-13 15:01:24 -0800

    Update Area method

--- src/Polygon.java
+++ src/Polygon.java
@@ -8,11 +8,6 @@ public class Polygon {
      * @author Aiden Michael Leon Guerrero San Nicolas
      */
 
-
-    /**
-     * @
-     *
-     */
     private int sides;
     private double sideLength;
     private String shapeType;
@@ -79,6 +74,22 @@ public class Polygon {
     }
 
 
+
+    public double getArea()
+    {
+        if(sides >= 3 && sideLength>1.0) {
+            double area = Math.round((sides * (sideLength * sideLength)) / (4.0 * Math.tan((Math.PI / sides)))*1000);
+            area /= 1000.0;
+            return area;
+        }
+
+        else{}
+        {
+            return area;
+        }
+
+    }
+
     //FOR THE AREA YOU HAVE TO MAKE AN IF STATEMENT
     //IF THE NUMBER OF SIDES ARE 3 THEN (if statement) DIVIDE (sideLength * sideLength) by two
     //for
@@ -89,7 +100,7 @@ public class Polygon {
     {
         DecimalFormat df = new DecimalFormat("#.##"); //displays a number with two deecimal places
         return "Your shape is a " + shapeType + " and it has " + df.format(sides) + " sides. \n" + " It has a side length of " + df.format(sideLength) + ".\n" +
-                " It has a perimeter of " + getPerimeter()  + " units.\n";
+                " It has a perimeter of " + getPerimeter()  + " units.\n" + " It has an area of " + getArea() + ".";
     }
 
 
