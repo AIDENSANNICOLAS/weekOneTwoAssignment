ae2aa93f0b5ba21b690a9f6fb46a3c210920bd76
AIDENSANNICOLAS <113197138+AIDENSANNICOLAS@users.noreply.github.com>
2023-02-15 08:35:20 -0800

    Update polygonTester.java

--- src/polygonTester.java
+++ src/polygonTester.java
@@ -1,21 +1,8 @@
-import java.util.Scanner;
-
 public class polygonTester {
 
     public static void main(String[]args)
     {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter the name of your Polygon: ");
-        String shapeType = input.nextLine();
-
-        System.out.println("Enter the number of sides your " + shapeType +  " has: ");
-        double sides = input.nextDouble();
-
-        System.out.println("Enter the length of your polygon's sides: ");
-        double sideLength = input.nextDouble();
-        
-        Polygon poly = new Polygon(sides, sideLength, shapeType);
+        Polygon poly = new Polygon(3, 2, "Triangle");
         System.out.println(poly);
 
     }
