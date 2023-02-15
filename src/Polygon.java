95b6d16c2ec4ea60cb925fcf313dde1dfab95313
AIDENSANNICOLAS <113197138+AIDENSANNICOLAS@users.noreply.github.com>
2023-02-15 08:35:05 -0800

    Update Polygon.java

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,4 +1,5 @@
 import java.text.DecimalFormat;
+import java.util.Arrays;
 
 public class Polygon {
     /**
@@ -7,46 +8,65 @@ public class Polygon {
      * @author Aiden Michael Leon Guerrero San Nicolas
      */
 
-    private double sides;
+    private int sides;
     private double sideLength;
     private String shapeType;
     private double area;
     private double perimeter;
 
 
-
+    /**
+     * @Polygon Default Polygon if no input from user.
+     */
     public Polygon()
     {
-        sides = 3.0;
+        sides = 3;
         sideLength = 1.0;
         shapeType = "Triangle";
         perimeter = sides * sideLength;
 
     }
 
-    public Polygon(double s, double sL, String sT)
+    public Polygon(int s, double sL, String sT)
     {
-        this.sides = s;
-        this.sideLength = sL;
-        this.shapeType = sT;
+        sides = s;
+        sideLength = sL;
+        shapeType = sT;
     }
 
+
+    /**
+     *
+     * @getNumSides This returns the number of sides the user inputs.
+     */
     public double getNumSides()
     {
         return sides;
     }
 
+
+    /**
+     *
+     * @getSideLength This returns the side Length to the user.
+     */
     public double getSideLength()
     {
         return sideLength;
     }
 
+    /**
+     *
+     * @return This returns the shapeType.
+     */
     public String getShapeType()
     {
         return shapeType;
     }
 
-
+    /**
+     *
+     * @return This returns the perimeter.
+     */
     public double getPerimeter()
     {
         perimeter = sides * sideLength;
@@ -56,26 +76,48 @@ public class Polygon {
 
     /**
      *
-     * @param newSideLength Allowing the User to change the side Length HIIII
+     * @param newSideLength Allowing the User to change the side Length
      */
 
-    public void setSideLength(double newSideLength)
+    public void setSideLength(int newSideLength)
     {
         sides = newSideLength;
     }
 
 
+    /**
+     *
+     * @return This returns the area for a triangle
+     */
+    public double getArea()
+    {
+        if(sides >= 3 && sideLength>1.0) {
+            double area = Math.round((sides * (sideLength * sideLength)) / (4.0 * Math.tan((Math.PI / sides)))*1000);
+            area /= 1000.0;
+            return area;
+        }
+
+        else{}
+        {
+            return area;
+        }
+
+    }
+
     //FOR THE AREA YOU HAVE TO MAKE AN IF STATEMENT
     //IF THE NUMBER OF SIDES ARE 3 THEN (if statement) DIVIDE (sideLength * sideLength) by two
     //for
 
-
+    /**
+     *
+     * @return This returns the text message after inputting information from the console.
+     */
 
     public String toString()
     {
         DecimalFormat df = new DecimalFormat("#.##"); //displays a number with two deecimal places
         return "Your shape is a " + shapeType + " and it has " + df.format(sides) + " sides. \n" + " It has a side length of " + df.format(sideLength) + ".\n" +
-                " It has a perimeter of " + getPerimeter()  + " units.\n";
+                " It has a perimeter of " + getPerimeter()  + " units.\n" + " It has an area of " + getArea() + ".";
     }
 
 
